3d41aa08a69db89eeae0bb7dcafedcb4cba79122
TylerTaka33 <student10@js-c02mxpt6g083.punahou.edu>
2015-07-08 11:44:50 -1000

    HelloHelloHelloHello

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 //Old macdonald had a C++, EIEIOOoOoOooooooooeoeoeOEOeEIABAIUSHMDoh*^5786548v86753V854876v58V87786v5c78v586CV87645V8¶√§£∞8√¶§∞8¶√§∞•¢√∞•¶£¢„·´‡˜9˜´·„°˜‡{Null ERROR}
+=======
+window.prompt("Welcome to Price$4Me, a website that allows people to post about items and how cheap they were");
+>>>>>>> Stashed changes
