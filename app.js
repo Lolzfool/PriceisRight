2b591f7bf65dead3cd078c6649c883f1e48683d2
TylerTaka33 <student10@js-c02mxpt6g083.punahou.edu>
2015-07-10 11:33:46 -1000

    gdfxfsagfdhdfgjythafdgfsehbfs

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 
-            var list = [];
-            function getText() {
-                var str = document.getElementById("txt");
-                list.push(str.value);
-                str.value = "";
-                str.focus();
-                var area = document.getElementById("stuff").innerHTML = list;
-                area.value = "";
-                for (var i = 0; i < list.length; i++) {
-                    area.value += list[i] + "\n";
-                 }
-             }
-    
+            // var list = [];
+            // function getText() {
+                // var str = document.getElementById("txt");
+                // list.push(str.value);
+                // str.value = "";
+                //str.focus();
+                //var area = document.getElementById("stuff").innerHTML = list;
+                // area.value = "";
+                // for (var i = 0; i < list.length; i++) {
+                    // str += list[i] + "<br>";
+                 // }
+                 // document.getElementById("stuff").innerHTML = str;
+             // }
+    (function($){
+    $('#myform').submit(function(e){
+        var val = $(this).find('#in').val();
+        $('ul.list').append('<li>' + val + '</li>');
+        e.preventDefault();
+    });
+})(jQuery);
