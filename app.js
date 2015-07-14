cfe65330c48ce0c64c393047c46391e8f7befd0c
TylerTaka33 <student10@js-c02mxpt6g083.punahou.edu>
2015-07-14 10:46:14 -1000

    newsfeed at top

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-
-            /* var list = [];
-            // function getText() {
-                // var str = document.getElementById("txt");
-                // list.push(str.value);
-                // str.value = "";
-                //str.focus();
-                //var area = document.getElementById("stuff").innerHTML = list;
-                // area.value = "";
-                // for (var i = 0; i < list.length; i++) {
-                    // str += list[i] + "<br>";
+        /* var list = [];
+             function getText() {
+                var str = document.getElementById("txt");
+                list.push(str.value);
+                str.value = "";
+                str.focus();
+                var area = document.getElementById("stuff").innerHTML = list;
+                 area.value = "";
+                 for (var i = 0; i < list.length; i++) {
+                     str += list[i] + "<br>";
                   }
                   document.getElementById("stuff").innerHTML = str;
               } */
     (function($){
     $('#myform').submit(function(e){
         var val = $(this).find('#in').val();
-        $('ul.list').append('<li>' + val + '</li>');
+        $('ul.list').prepend('<li>' + val + '</li>');
         e.preventDefault();
     });
 })(jQuery);
